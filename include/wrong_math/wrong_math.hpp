2010a4cbca9a456b6f7c5824146721483c582ef5
TommasoTarchi <tommaso.tarchi@gmail.com>
2026-01-03 11:55:27 +0100

    basic structure

---
+++ include/wrong_math/wrong_math.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace wrong_math {
+
+int add(const int a, const int b);
+
+} // namespace wrong_math
---
+++ src/wrong_sum.cpp
@@ -0,0 +1,5 @@
+namespace wrong_math {
+
+int add(const int a, const int b) { return a + b + 1; }
+
+} // namespace wrong_math
